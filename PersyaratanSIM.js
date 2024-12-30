da7dd8ebb52e782278c59489d62ceedba619d2ea
DERMANRAY <rizkydermawan12345@gmail.com>
2024-12-30 19:31:18 +0700

    Add files via upload

---
+++ PersyaratanSIM.js
@@ -0,0 +1,13 @@
+/**
+ * membuat stdy case dengan perizinan berkendara, jika umur 17+ dan mempunyai sim, maka izinkan berkendara
+ */
+
+let InputUmur = 12 //input umur disini
+let ApakahPunyaSIM = true
+
+if (InputUmur >= 17 && ApakahPunyaSIM) {
+    console.log ("ANDA DIIZINKAN BERKENDARA")
+}
+else {
+    console.log ("ANDA TIDAK DIIZINKAN BERKENDARA")
+}
